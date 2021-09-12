fe91fdc52d3b7dc56530fab0a60759d62acf7df5
mrfox92 <davidalvarez54@gmail.com>
2021-09-11 21:20:12 -0300

    Agregada validacion discapacidades PSD

--- app/Http/Requests/DiscapacidadPSDRequest.php
+++ app/Http/Requests/DiscapacidadPSDRequest.php
@@ -34,7 +34,7 @@ class DiscapacidadPSDRequest extends FormRequest
                     'nombre'                    =>  'required',
                     'observacion'               =>  'nullable|max:500',
                     'porcentaje_discapacidad'   =>  'nullable',
-                    'picture'                   =>  'sometimes|image|mimes:jpg,jpeg,png',
+                    'picture'                   =>  'sometimes|image|mimes:jpg,jpeg,png|max:2048',
                 ];
             case 'PUT':
                 return [
@@ -42,7 +42,7 @@ class DiscapacidadPSDRequest extends FormRequest
                     'nombre'                    =>  'required',
                     'observacion'               =>  'nullable|max:500',
                     'porcentaje_discapacidad'   =>  'nullable',
-                    'picture'                   =>  'sometimes|image|mimes:jpg,jpeg,png',
+                    'picture'                   =>  'sometimes|image|mimes:jpg,jpeg,png|max:2048',
                 ];
         }
     }
@@ -58,14 +58,16 @@ class DiscapacidadPSDRequest extends FormRequest
                     'tipo_discapacidad_id.required'     =>  'Campo obligatorio, por favor seleccione una opción',
                     'nombre.required'                   =>  'Campo obligatorio, por favor ingrese un nombre discapacidad',
                     'observacion.max'                   =>  'El texto es demasiado largo, debe tener un máximo de 500 caracteres',
-                    'picture.image'                     =>  'El archivo a subir debe tener un formato de imagen válido, pruebe subir una imagen con extension jpg, jpeg o png'
+                    'picture.image'                     =>  'El archivo a subir debe tener un formato de imagen válido, pruebe subir una imagen con extension jpg, jpeg o png',
+                    'picture.max'                       =>  'Debe subir una imagen con un peso no superior a los 2 MB, por favor reintente con una imagen más liviana',
                 ];
             case 'PUT':
                 return [
                     'tipo_discapacidad_id.required'     =>  'Campo obligatorio, por favor seleccione una opción',
                     'nombre.required'                   =>  'Campo obligatorio, por favor ingrese un nombre discapacidad',
                     'observacion.max'                   =>  'El texto es demasiado largo, debe tener un máximo de 500 caracteres',
-                    'picture.image'                     =>  'El archivo a subir debe tener un formato de imagen válido, pruebe subir una imagen con extension jpg, jpeg o png'
+                    'picture.image'                     =>  'El archivo a subir debe tener un formato de imagen válido, pruebe subir una imagen con extension jpg, jpeg o png',
+                    'picture.max'                       =>  'Debe subir una imagen con un peso no superior a los 2 MB, por favor reintente con una imagen más liviana',
                 ];
         }
     }
